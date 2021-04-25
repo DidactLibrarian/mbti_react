f61ea38fec0b6352c2b89fc6d6585b61c4d8d3bd
SeongHyun Park <okpc0306@naver.com>
2021-04-26 01:42:30 +0900

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+//TESTPAGE
+const testReady = "/"; //HOME
+const testProcess = "/process"; //PROCESS
+const testResult = "/result"; //RESULT
+
+const routes = {
+  TESTREADY: testReady,
+  TESTPROCESS: testProcess,
+  TESTRESULT: testResult,
+};
+
+export default routes;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as TestReady } from "./TestReady";
+export { default as TestProcess } from "./TestProcess";
+export { default as TestResult } from "./TestResult";
